cea430e6acde9d6a13e9fbb55fa1eed46752b9bc
Wladimir Chopite <wchopite@gmail.com>
2017-03-04 19:00:53 -0400

    string interpolation example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Inline Template</h1>
-    <fa-other></fa-other>
-    <fa-another>
-      <div>
-        <h1>Hello!</h1>
-        <p>World</p>
-      </div>
-    </fa-another>
-    <fa-another>
-      Something else
-    </fa-another>
+    <h1>Root Component</h1>
+    <fa-databinding></fa-databinding>
   `,
   styles: [`
     h1 {
